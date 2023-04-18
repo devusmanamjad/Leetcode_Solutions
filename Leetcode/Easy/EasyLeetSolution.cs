f64795ce890f9b74d181f41543fa878b47a58544
Muhammad Usman Amjad <muhammadusmanamjad1@gmail.com>
2023-04-18 23:32:27 +0500

    66. Plus One

--- Leetcode/Easy/EasyLeetSolution.cs
+++ Leetcode/Easy/EasyLeetSolution.cs
@@ -170,6 +170,26 @@ namespace Leetcode.Easy
 
         #endregion
 
+        #region
+        public static int[] PlusOne(int[] digits)
+        {
+            int n = digits.Length;
+            for (int i = n - 1; i >= 0; i--)
+            {
+                if (digits[i] < 9)
+                {
+                    digits[i]++;
+                    return digits;
+                }
+                digits[i] = 0;
+            }
+            // If all digits were 9, we need to add an extra digit at the beginning
+            int[] newDigits = new int[n + 1];
+            newDigits[0] = 1;
+            return newDigits;
+        }
+
+        #endregion
 
     }
 }
